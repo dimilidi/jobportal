c2a16943a83d4c100564ec39a1b72c341310be44
lididimi <dimitrovalidia@yahoo.com>
2022-12-08 13:33:59 +0100

    controllers aufgesetzt

---
+++ backend/src/controller/userController.js
@@ -0,0 +1,29 @@
+import httpErrors from 'http-errors'
+
+
+
+
+/** @type {import("express").RequestHandler} */
+export function getUser (req, res) {
+  throw httpErrors.NotImplemented()
+}
+
+
+
+/** @type {import("express").RequestHandler} */
+export async function register (req, res) {
+  throw httpErrors.NotImplemented()
+}
+
+
+/** @type {import("express").RequestHandler} */
+export async function login (req, res) {
+  throw httpErrors.NotImplemented()
+}
+
+
+
+/** @type {import("express").RequestHandler} */
+export async function updateUser (req, res) {
+  throw httpErrors.NotImplemented()
+}
