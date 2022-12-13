714a81c8bdd2ac8e575eeaae779dd5963afe2379
lididimi <dimitrovalidia@yahoo.com>
2022-12-13 09:25:29 +0100

    footer fertig

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
+import Footer from "./Components/Footer"
+import AdsList from "./Pages/AdsList"
+
 
 function App() {
 
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <AdsList />
+      <Footer/>
+    </>
+   
   )
 }
 
