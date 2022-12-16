74a468072672cd992981cf26407b05b5831fab97
lididimi <dimitrovalidia@yahoo.com>
2022-12-16 11:38:00 +0100

    footer

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -3,11 +3,11 @@ type Props = {}
 
 function Footer({}: Props) {
   return (
-    <div className='mx-auto flex justify-center align-middle'>
-      <div className='p-4 w-full fixed bottom-0 z-10 rounded-t-[40rem] opacity-90 bg-darkBeige md:w-4/5'>
+    <footer className='mx-auto w-full flex justify-center align-middle sm:fixed sm:bottom-0 '>
+      <div className='w-full  p-4  bottom-0 sm:left-0 z-10 rounded-t-[40rem] opacity-90 bg-darkBeige  md:w-4/5  '>
         <p className="text-l text-center text-gray text-opacity-30 font-light"> &copy; 2022 All rigths reserved <span className='font-bold'> Jobsy.</span></p>
       </div>
-    </div>
+    </footer>
    
   )
 }
