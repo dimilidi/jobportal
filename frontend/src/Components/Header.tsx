2ccd25eb79c22bab082b9db4006716a7c6e409c1
lididimi <dimitrovalidia@yahoo.com>
2022-12-16 11:31:40 +0100

    header

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -6,15 +6,14 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <section className="header flex items-baseline  justify-between p-6 ">
+    <header className="p-4 w-screen h-70px top-0 right-0 fixed flex items-center  justify-between bg-background">
       <div className=' bg-lightGreen rounded-[100%] w-[34px] h-[34px] absolute -left-5 '></div>
 
         <Link to='/adslist'>
           <h2 className="pl-8 font-extrabold tracking-wide text-lg ">Jobsy.</h2> 
         </Link>
-
-          <Navigation />    
-    </section>
+       <Navigation />    
+    </header>
   )
   }
 
