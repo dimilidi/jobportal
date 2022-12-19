e400c9dfde9a5b3745a3762231abdf724f822951
lididimi <dimitrovalidia@yahoo.com>
2022-12-19 02:21:37 +0100

    added useAds hook

--- frontend/src/Hooks/useAds.tsx
+++ frontend/src/Hooks/useAds.tsx
@@ -1,6 +1,9 @@
 import React, { useState,  useEffect } from 'react'
 import axios from 'axios'
 import axiosInstance from '../api/axiosInstance'
+import { useParams } from 'react-router-dom'
+import useUser from './useUser'
+
 
 
 type Ad = {
@@ -8,28 +11,48 @@ type Ad = {
     title: string
     category: string
     description: string
+    location:string
     wage: number
     contactVia: string
-    createdAt: string
+    createdAt: Date
+    user: {
+        name:string
+        email:string
+        phone: string
+       
+    }
 }
 
 type AdHook= {
-    // list: Ad[]
-    // min: number
-    // max: number
-    // band: string
-    // setMin: (min: number) => void 
-    // setMax: (max:number) => void
-    // setBand: (band: string) => void
+    list: Ad[]
+    ad: Ad | null
+    isLoading:boolean
+    error:string
+    setIsLoading: (isLoading:boolean) => void
+    setError: (error:string) => void
 }
 
 
 
 function useAds():AdHook{
-    const [list, setList]   = useState<Ad[]>([])
-    
+ 
+    const params = useParams()
+
+    const [list, setList] = useState<Ad[]>([])
+    const [ad, setAd] = useState<Ad | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState('')
+
+
+    useEffect(() => {
+        fetchAds()
+    }, [])
+
+ 
     const fetchAds = async() => {
-        try {
+       
+        try { 
+            setIsLoading(true)
             const res = await axiosInstance.get('/ads');
             const data = res.data;
             console.table("data /ads" + data);
@@ -37,18 +60,30 @@ function useAds():AdHook{
         } catch (error) {
             console.log(error)
         }
+        setIsLoading(false)
+    }
+
+
+    const getAdById = async() => {
+        try {
+            setIsLoading(true)
+            const res = await axiosInstance.get(`./ads/${params.id}`)
+            const data = res.data
+            // console.log(`data/ads/${params.id}`, data)
+            setAd(data)
+        } catch (error) {
+            console.log(error)
+        }
+        setIsLoading(true)
     }
 
     useEffect(() => {
-        fetchAds()
-    }, [])
+        getAdById()
+    },[params.id])
 
-    console.log('HALLLLLLOOOOOO');
-    
 
-  return (
-    {}
-  )
+
+  return ({list,ad, setIsLoading, setError, error, isLoading})
 }
 
 export default useAds
