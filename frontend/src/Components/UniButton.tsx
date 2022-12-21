a0116d5748fbaa2109294191180037241be3a0ce
lididimi <dimitrovalidia@yahoo.com>
2022-12-21 16:54:57 +0100

    home almost ready. Lines to be done

--- frontend/src/Components/UniButton.tsx
+++ frontend/src/Components/UniButton.tsx
@@ -1,5 +1,6 @@
 type Props = {
   text: string
+  type?: string
   onClick?: () => void
   fontSize?: string
   width?: string
@@ -12,7 +13,8 @@ const UniButton = (props: Props) => {
     <div className={props.className}>
       <button
         onClick={props.onClick}
-        className='w-[100%] flex items-center justify-center rounded-full bg-lightGreen text-white h-[40px] hover:bg-darkGreen  ease-in-out duration-300 md:w-[230px] px-[24px] py-[12px] fontSize-[16px]'
+        className=' w-[100%] flex items-center justify-center rounded-full bg-lightGreen text-white  hover:bg-darkGreen  ease-in-out duration-300 px-[24px] py-[12px] fontSize-[16px] shadow-lg'
+        // className='w-[100%] flex items-center justify-center rounded-full bg-lightGreen text-white h-[40px] hover:bg-darkGreen  ease-in-out duration-300 md:w-[230px] px-[24px] py-[12px] fontSize-[16px]'
         style={{
           fontSize: props.fontSize,
           width: props.width,
