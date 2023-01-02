8723577e87e81e0b5f94aeb7702cbd11896ffe05
Orianaqh <orianaqh1583@gmail.com>
2023-01-02 13:16:01 +0100

    änderungen accountSeite

--- frontend/src/Pages/Account.tsx
+++ frontend/src/Pages/Account.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
-import Header from '../Components/Header'
+import UniButton from '../Components/UniButton'
+import imageAccount from '../assets/images/Account_profilDefault.png'
+import { MdMail , MdCall, MdLocationOn} from "react-icons/md"
+import Ad from '../Components/Ad'
+import useAds from '../Hooks/useAds'
+
 
 type Props = {}
 
@@ -9,19 +13,17 @@ const handleClick = async () => {
 }
 
 const Account = (props: Props) => {
+  const ads = useAds()
   return (
     <>
       {/* Grid Container For Whole Page */}
       <div
         className=' 
-      mb-[4rem]
-      pt-[70px]
       md:pt-[100px]
       w-[100%] md:h-[90vh]
       grid
       bg-background
       sm:grid-cols-1 md:grid-cols-2
-
       '
       >
         {/* User Card Container */}
@@ -29,8 +31,8 @@ const Account = (props: Props) => {
           className='
         max-w-[450px] m-4 p-2
         flex flex-col justify-center relative  
-        overflow-hidden 
-        border rounded-bl-[65px] rounded-br-[65px]
+        overflow-hidden shadow-standard 
+        rounded-bl-[65px] rounded-br-[65px]
         bg-white
         sm:justify-center md:mb-0  lg:justify-self-center lg:mb-[5.5rem]
         '
@@ -43,10 +45,10 @@ const Account = (props: Props) => {
           >
             <div
               className='
-            w-[100%] h-[8em] 
+            w-[100%] h-[13em] 
             flex justify-center absolute -top-4 
-            rounded-bl-full rounded-br-full
-          bg-lightGreen '
+            rounded-b-[200px]
+            bg-darkBeige'
             ></div>
           </div>
           {/* .................... */}
@@ -60,21 +62,19 @@ const Account = (props: Props) => {
           >
             <img
               className='
-            w-[8em] h-[8em] mt-6 
+            w-[12em] h-[12em] mt-[-2rem] 
             absolute z-10 
-            border rounded-full 
-            bg-lightBeige'
-              src=''
-              alt='User Avatar'
+            rounded-full'
+              src={imageAccount}
             />
           </div>
 
           {/* User Name-, and Section */}
-          <div className='mt-[4em] mx-1'>
+          <div className='mt-[5em]'>
             <h1
               className='
               flex justify-center 
-              font-[700] text-[30px]
+              font-[700] text-[2.5rem]
               '
             >
               Viktoria Schulz
@@ -83,42 +83,41 @@ const Account = (props: Props) => {
               className='
               mb-6 
               flex justify-center 
-              text-[16px]'
+              text-[1.8rem] font-light'
             >
               Web-Developer
             </h2>
           </div>
 
           {/* Description Heading and Description */}
-          <div className='min-[374px]:mx-[1em]'>
+          <div className='pl-9 pr-9'>
             <h3 className='mb-1 text-[16px] font-[600]'>Description</h3>
             <p className='text-[15px] text-[gray]'>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Obcaecati harum omnis sapiente eum illo ratione sunt dignissimos
-              dolorem animi delectus sit voluptatem aperiam maxime provident
-              natus, totam consequuntur pariatur suscipit?
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.Ob caecati harum omnis sapiente eum illo ratione sunt dignissimos dolorem animi delectus sit voluptatem aperiam maxime provident natus, totam consequuntur pariatur suscipit.
             </p>
           </div>
 
           {/* Contact Data */}
           <div
             className='
+          pl-5 pr-5
           my-6 
-          text-[15px] font-[800] 
+          text-[17px] font-[500] 
           min-[374px]:mx-[1em]'
           >
-            <div className=''>email@email.com</div>
-            <div className=''>0761/384059234</div>
-            <div className=''>79111 Freiburg</div>
+            <div className='flex flex-row gap-4 items-center text-gray leading-relaxed'> <MdMail/> email@email.com</div>
+            <div className='flex flex-row gap-4 items-center text-gray leading-relaxed'> <MdCall/> 0761/384059234</div>
+            <div className='flex flex-row gap-4 items-center text-gray leading-relaxed'> <MdLocationOn/> 79111 Freiburg</div>
           </div>
 
           {/* Edit Profile Button Wrapper */}
           <div
-            className='
-          mb-4 
-          flex justify-center'
+            className='flex justify-center'
           >
-            <button>Edit Profile</button>
+            <UniButton
+            area-label='EditProfileButton'
+            text= 'Edit Profile'
+            />
           </div>
         </div>
 
@@ -130,9 +129,9 @@ const Account = (props: Props) => {
           <p className='min-w-[850px]:absolute mt-8 mb-4 text-center hidden md:mt-0'>
             You have no Ads yet
           </p>
-          <div className='w-[100%] flex justify-evenly'>
+          <div className='w-[100%] flex justify-evenly gap-6'>
             <button
-              className='py-2 border-2 rounded-3xl border-lightGreen basis-1/2'
+              className='py-2 border-2 rounded-3xl border-lightGreen  basis-1/2 bg-white'
               onClick={handleClick}
             >
               Post Ad
@@ -145,18 +144,26 @@ const Account = (props: Props) => {
             </button>
           </div>
 
-          {/* Horizontal Line */}
-          <hr className='my-6' />
+        {/* Horizontal Line */}
+        <div
+        area-label='line'
+        className='w-[20%] hidden md:block md:absolute md:top-[13%] md:translate-y-[-50%] md:right-0 md:border-b-[3px] md:border-lightGreen'
+        />
 
-          {/* List Box Container */}
-          <div className=''>
-            <h3 className='mb-2 text-center text-lg font-semibold'>Your Ads</h3>
-            <ul className='p-2 m-2 mb-10 border rounded-xl'>
-              <li className='mb-4'>Ad 1</li>
-              <li className='mb-4'>Ad 2</li>
-              <li className='mb-4'>Ad 3</li>
-            </ul>
+        {/* List Box Container */}
+        <div className='flex justify-start flex-col'>
+          <h3 className='text-lg font-semibold text-gray'>Your Ads</h3>
+
+        {/* Ads Container */}
+        <div className='mt-[30px] w-full h-full flex flex-wrap p-10 justify-items-center items-start rounded-[21px] bg-white shadow-standard  sm:px-5 sm:overflow-y-scroll'>
+          {/* Ads */}
+          <div className='mx-auto flex flex-wrap justify-center '>
+            {ads.list.map((ad) => (
+              <Ad key={ad._id} ad={ad} />
+            ))}
           </div>
+        </div>
+        </div>
         </div>
       </div>
     </>
