1c65e83ad2dd673076ee8219bbac4cb475b4cac4
lididimi <dimitrovalidia@yahoo.com>
2023-01-03 15:44:51 +0100

    .

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
 
-const BASE_URL = 'https://jobsy-zvxm.onrender.com'
+const BASE_URL = 'http://localhost:3001'
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
