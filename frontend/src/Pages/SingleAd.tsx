ff23836f8bf6fb00475dec8a98c4240c32ca6557
M-123-456 <mikigerlach2@gmail.com>
2023-01-03 10:03:57 +0100

    Single-Ad

--- frontend/src/Pages/SingleAd.tsx
+++ frontend/src/Pages/SingleAd.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, Link } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import ContactDetails from '../Components/ContactDetails'
 import UniButton from '../Components/UniButton'
 import AdMobile from '../Components/AdMobile'
@@ -12,9 +12,7 @@ import Spinner from '../Components/Spinner'
 
 import useUser from '../Hooks/useUser'
 import useAds from '../Hooks/useAds'
-import JobsButton from '../Components/JobsButton'
-import BigCircle from '../Components/background/BigCircle'
-import Line from '../Components/background/Line'
+import BackButton from '../Components/BackButton'
 
 type Props = {}
 
@@ -33,21 +31,28 @@ const SingleAd = (props: Props) => {
   if (ads.isLoading) {
     return <Spinner />
   }
+  console.log(ads)
 
   return (
     <div
       area-label='page-singleAd'
       className='pt-[110px] relative flex justify-center text-Black'
     >
-      <BigCircle />
-      <Line />
+      <div
+        area-label='circle'
+        className='hidden lg:block lg:w-[332px] lg:h-[332px] lg:fixed lg:top-[50%] lg:left-[-250px] lg:translate-y-[-50%]  lg:rounded-full lg:bg-lightGreen'
+      />
+      <div
+        area-label='line'
+        className='hidden lg:block lg:fixed lg:top-[50%] lg:translate-y-[-50%] lg:left-0 lg:border-b-2 lg:border-lightGreen w-screen'
+      />
 
       {/* Main part of single ad */}
       <div
         area-label='main'
         className='w-full sm:w-[80%] flex flex-col justify-around gap-5 lg:justify-center lg:w-[60%]'
       >
-        <JobsButton />
+        <BackButton />
 
         {/* Ad */}
         <div
@@ -57,31 +62,12 @@ const SingleAd = (props: Props) => {
           <AdMobile />
           <div area-label='description' className='mt-3'>
             <h3 className='text-[20px]'>Description</h3>
-            <p className='mt-2 text-gray/80'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-              dignissimos expedita delectus ipsa, tempore, fugiat quae inventore
-              dicta nihil aut, amet officia maiores eveniet? Neque nemo
-              assumenda, incidunt eaque vero error, velit in optio voluptas nam
-              impedit ab nostrum qui modi sequi, cum architecto quaerat
-              laboriosam perspiciatis quo distinctio ea voluptatum. Magnam hic
-              inventore iusto eveniet voluptates libero nemo, commodi itaque
-              enim voluptatem in. Eum in odit vel, consequuntur labore debitis
-              officiis ad quia ducimus provident tenetur voluptatum laboriosam
-              nulla dolorum fugit. Modi corrupti necessitatibus perspiciatis
-              temporibus ipsam doloremque molestiae similique explicabo ea!
-              Dignissimos ipsum atque voluptate, placeat officia ipsa. Lorem
-              ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-              dignissimos expedita delectus ipsa, tempore, fugiat quae inventore
-              dicta nihil aut, amet officia maiores eveniet? Neque nemo
-              assumenda, incidunt eaque vero error, velit in optio voluptas nam
-              impedit ab nostrum qui modi sequi, cum architecto quaerat
-              laboriosam perspiciatis quo distinctio ea voluptatum.
-            </p>
+            <p className='mt-2 text-gray/80'>{ads.ad?.description}</p>
           </div>
         </div>
         {/* Ad - END */}
 
-        {/* If user exists, show ContactDetails */}
+        {/* ContactDetails DESKTOP - If user exists, show ContactDetails */}
         {user.user && (
           <ContactDetails className='hidden lg:w-[200px] lg:h-[300px] lg:flex lg:items-center lg:fixed lg:right-0 lg:top-[50%] lg:rounded-l-[65px] lg:translate-y-[-50%] xl:w-[230px]' />
         )}
@@ -90,6 +76,8 @@ const SingleAd = (props: Props) => {
           onClick={handleClick}
           className='my-5 self-center'
         />
+
+        {/* ContactDetails MOBILE - If user exists, show ContactDetails */}
         {user.user && (
           <ContactDetails className='w-[258px] h-[287px] flex justify-center self-center rounded-t-[65px] lg:hidden' />
         )}
