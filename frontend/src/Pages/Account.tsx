c64d606e756b0be39f4033f1319b9332acbbb301
Orianaqh <orianaqh1583@gmail.com>
2023-01-03 14:32:43 +0100

    userCard

--- frontend/src/Pages/Account.tsx
+++ frontend/src/Pages/Account.tsx
@@ -5,6 +5,7 @@ import { MdMail , MdCall, MdLocationOn} from "react-icons/md"
 import Ad from '../Components/Ad'
 import useAds from '../Hooks/useAds'
 import UniButtonWhite from '../Components/UniButtonWhite'
+import UserCard from '../Components/UserCard'
 
 type Props = {}
 
@@ -21,102 +22,8 @@ const Account = (props: Props) => {
           md:flex-col lg:flex-row'
       >
 
-      {/* User Card Container */}
-      <div
-          className='mt-[30%]
-            min-w-[375px]
-            max-w-[450px]
-            flex flex-col justify-center relative  
-            shadow-standard 
-            rounded-bl-[65px] rounded-br-[65px]
-            bg-white
-            sm:justify-center lg:mt-[10%] lg:justify-self-center
-            '
-        >
-          {/* Avatar BG Halfcircle */}
-          <div
-            className='
-            flex justify-center'
-          >
-            <div
-              className='
-            w-[100%] h-[13em] 
-            flex justify-center absolute top-0 
-            rounded-b-[200px]
-            bg-darkBeige'
-            ></div>
-          </div>
-          {/* .................... */}
-
-          {/* Avatar Circle Container */}
-          <div
-            className='
-            w-[100%] h-[6em] 
-            flex justify-center
-          '
-          >
-            <img
-              className='
-            w-[12em] h-[12em] mt-[2rem] 
-            absolute z-10 
-            rounded-full'
-              src={imageAccount}
-            />
-          </div>
-
-          {/* User Name-, and Section */}
-          <div className='mt-[9em]'>
-            <h1
-              className='
-              flex justify-center 
-              font-[700] text-[2.5rem]
-              '
-            >
-              Viktoria Schulz
-            </h1>
-            <h2
-              className='
-              mb-6 
-              flex justify-center 
-              text-[1.8rem] font-light'
-            >
-              Web-Developer
-            </h2>
-          </div>
-
-          {/* Description Heading and Description */}
-          <div className='pl-9 pr-9'>
-            <h3 className='mb-1 text-[16px] font-[600]'>Description</h3>
-            <p className='text-[15px] text-[gray]'>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.Ob caecati harum omnis sapiente eum illo ratione sunt dignissimos dolorem animi delectus sit voluptatem aperiam maxime provident natus, totam consequuntur pariatur suscipit.
-            </p>
-          </div>
-
-          {/* Contact Data */}
-          <div
-            className='
-          pl-5 pr-5
-          my-6 
-          text-[17px] font-[500] 
-          min-[374px]:mx-[1em]'
-          >
-            <div className='flex flex-row gap-4 items-center text-gray leading-relaxed'> <MdMail/> email@email.com</div>
-            <div className='flex flex-row gap-4 items-center text-gray leading-relaxed'> <MdCall/> 0761/384059234</div>
-            <div className='flex flex-row gap-4 items-center text-gray leading-relaxed'> <MdLocationOn/> 79111 Freiburg</div>
-          </div>
-
-          {/* Edit Profile Button Wrapper */}
-          <div
-            className='flex justify-center mb-10'
-          >
-            <UniButton
-            area-label='EditProfileButton'
-            text= 'Edit Profile'
-            type='button'
-            className='sm:w-[45%] md:w-[55%] lg:w-[30%] xl:w-[50%]'
-            />
-          </div>
-        </div>
+        {/* User Card */}
+        <UserCard />
 
         {/* ############################################# */}
 
@@ -169,7 +76,7 @@ const Account = (props: Props) => {
           <UniButton
             text='Browse Ads'
             type='button'
-            className='z-10 sm:w-[80%] lg:w-[50%] xl:w-[35%]'
+            className='z-10 lg:w-[50%] xl:w-[35%]'
             onClick={() => navigate('/adslist')}
           />
           </div>
