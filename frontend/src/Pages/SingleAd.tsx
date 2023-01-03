a214c363158cd167d6b2cd42e73fb20442a71268
M-123-456 <mikigerlach2@gmail.com>
2023-01-03 12:10:57 +0100

    singleAd style fine-tuning

--- frontend/src/Pages/SingleAd.tsx
+++ frontend/src/Pages/SingleAd.tsx
@@ -36,8 +36,47 @@ const SingleAd = (props: Props) => {
   return (
     <div
       area-label='page-singleAd'
-      className='h-full min-h-[1150px] relative flex justify-center items-center  text-Black lg:min-h-[920px] lg:pt-0'
+      className='pt-[10%] h-full  flex flex-col items-center text-textBlack md:min-h-[800px]'
     >
+      <div area-label='wrapper' className='flex justify-center'>
+        {/* Main part of single ad */}
+
+        <div
+          area-label='main'
+          className='w-full sm:w-[80%] flex flex-col justify-between gap-5 lg:justify-center lg:w-[60%]'
+        >
+          <BackButton />
+
+          {/* Ad */}
+          <div
+            area-label='ad'
+            className='py-5 px-9 mt-8 mx-8 min-w-[270px] flex flex-col item-center z-10 rounded-[21px] bg-white shadow-standard sm:mx-0 lg:min-h-[400px] '
+          >
+            <AdMobile />
+            <div area-label='description' className='mt-3'>
+              <h3 className='text-[20px]'>Description</h3>
+              <p className='mt-2 text-gray/80'>{ads.ad?.description}</p>
+            </div>
+          </div>
+          <UniButton
+            text='Contact'
+            onClick={handleClick}
+            className='my-7 self-center lg:mb-0'
+          />
+        </div>
+        {/* Ad - END */}
+
+        {/* Main part of single ad - END */}
+      </div>
+
+      {/* ContactDetails MOBILE - If user exists, show ContactDetails */}
+      {user.user && (
+        <ContactDetails className='w-[258px] h-[287px] flex justify-center self-center rounded-t-[65px] lg:hidden' />
+      )}
+
+      {/* fixed or absolute elements - Circle, Line, ContactDetails in Desktop, Image, Toaster */}
+
+      {/* Circle and line in the background */}
       <div
         area-label='circle'
         className='hidden lg:block lg:w-[332px] lg:h-[332px] lg:fixed lg:top-[50%] lg:left-[-250px] lg:translate-y-[-50%]  lg:rounded-full lg:bg-lightGreen'
@@ -46,55 +85,25 @@ const SingleAd = (props: Props) => {
         area-label='line'
         className='hidden lg:block lg:fixed lg:top-[50%] lg:translate-y-[-50%] lg:left-0 lg:border-b-2 lg:border-lightGreen w-screen'
       />
-
-      {/* Main part of single ad */}
-
-      <div
-        area-label='main'
-        className='w-full h-full  sm:w-[80%]  min-h-[920px] flex flex-col justify-between gap-5 lg:justify-center lg:w-[60%]'
-      >
-        <BackButton />
-
-        {/* Ad */}
-        <div
-          area-label='ad'
-          className='py-5 px-9 mt-8 mx-8 min-w-[270px] flex flex-col item-center z-10 rounded-[21px] bg-white shadow-standard sm:mx-0 lg:min-h-[400px] '
-        >
-          <AdMobile />
-          <div area-label='description' className='mt-3'>
-            <h3 className='text-[20px]'>Description</h3>
-            <p className='mt-2 text-gray/80'>{ads.ad?.description}</p>
-          </div>
-        </div>
-        <UniButton
-          text='Contact'
-          onClick={handleClick}
-          className='my-7  w-[250px]  self-center lg:mb-0'
-        />
-      </div>
-      {/* Ad - END */}
+      {/* Circle and line in the background - END */}
 
       {/* ContactDetails DESKTOP - If user exists, show ContactDetails */}
       {user.user && (
         <ContactDetails className='hidden lg:w-[200px] lg:h-[300px] lg:flex lg:items-center lg:fixed lg:right-0 lg:top-[50%] lg:rounded-l-[65px] lg:translate-y-[-50%] xl:w-[230px]' />
       )}
-
-      {/* ContactDetails MOBILE - If user exists, show ContactDetails */}
-      {user.user && (
-        <ContactDetails className='w-[258px] h-[287px] flex justify-center self-center rounded-t-[65px] lg:hidden' />
-      )}
-
-      {/* Main part of single ad - END */}
+      {/* ContactDetails - END */}
 
       {/* image */}
       <img
         src={thinkingGirl}
         alt='illustration of girl in front of laptop'
-        className='hidden lg:w-[230px] lg:h-[220px] lg:absolute lg:bottom-[30px] lg:left-[14%] lg:block lg:z-30'
+        className='hidden lg:w-[230px] lg:h-[220px] lg:absolute lg:bottom-[10px] lg:left-[14%] lg:block lg:z-30'
       />
       {/* image - END */}
 
       <ToastContainer position='bottom-right' />
+
+      {/* fixed or absolute elements - Circle, Line, ContactDetails in Desktop, Image, Toaster - END */}
     </div>
   )
 }
