2f88a43bd4e8260d26f9a8496bedfc957fdfe92f
lididimi <dimitrovalidia@yahoo.com>
2023-01-05 22:08:12 +0100

    .

--- frontend/src/Components/ContactDetails.tsx
+++ frontend/src/Components/ContactDetails.tsx
@@ -18,26 +18,26 @@ const ContactDetails = (props: Props) => {
       {/* GREEN CONTACT BOX */}
       <div
         area-label='contact-data'
-        className='px-[30px] py-[30px] 
+        className='px-[20px] py-[20px] 
         flex flex-col items-center justify-center gap-3 rounded-[inherit] 
         text-gray bg-lightBeige xl:bg-lightGreen xl:text-white
         w-[100%]
         lg:items-start'
       >
         {/* NAME */}
-        <h3 className='self-center text-xl font-bold text-center w-full'>
+        <h3 className='p-1 self-center text-xl font-bold text-center w-full'>
           {ads.ad?.user.name}
         </h3>
 
         {/* EMAIL, PHONE, CITY */}
         <div
           area-label='box-with-contact-details'
-          className='py-2 flex flex-col gap-3 xl:px-1 md:gap-2 sm:p-0 w-full'
+          className=' py-2 flex flex-col gap-3 xl:px-1 md:gap-2 sm:p-0 w-full'
         >
           {/* if email is set as a contact data, show email */}
           {ads.ad?.contactVia.includes('email') && (
             <div className='mx-auto w-full flex items-center'>
-              <div className='p-1'>
+              <div className='p-2'>
                 <MdEmail className='text-lg ' />
               </div>
               <p className=' w-full ml-2 text-sm break-all md:py-1'>
