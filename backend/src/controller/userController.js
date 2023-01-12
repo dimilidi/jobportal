d441c955262d629b4ef66218c7398f79bfc07dee
lididimi <dimitrovalidia@yahoo.com>
2023-01-12 10:59:38 +0100

    .

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -72,8 +72,15 @@ export async function editAccount(req, res) {
   })
   // Change avatar
   if (avatar) {
-    const upload = await cloudinary.v2.uploader.upload(avatar)
-    user.avatar = upload.secure_url
+    const upload = await cloudinary.v2.uploader
+      // eslint-disable-next-line no-undef
+      .upload(avatar, {folder: avatars, width: 150, crop: 'scale'})
+
+    user.avatar = {
+      public_id: upload.public_id,
+      secure_url: upload.secure_url
+    }
+    // user.avatar = upload.secure_url
   }
 
   // Change other items
