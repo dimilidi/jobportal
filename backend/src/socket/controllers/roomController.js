7228ab3a0591132425e3cd17de87a740cc47897a
lididimi <dimitrovalidia@yahoo.com>
2023-01-21 20:59:15 +0100

    chat v1

--- backend/src/socket/controllers/roomController.js
+++ backend/src/socket/controllers/roomController.js
@@ -1,11 +1,11 @@
-import BaseController from './BaseController.js'
+// import BaseController from './BaseController.js'
 
 
-export default class RoomController extends BaseController {
+// export default class RoomController extends BaseController {
 
-  joinRoom = (id) => {
-    console.log(id, "has entered the chat")
-    this.socket.join(id)
-  }
+//   joinRoom = (id) => {
+//     console.log(id, "has entered the chat")
+//     this.socket.join(id)
+//   }
   
-}
+// }
