4891abd25bd2a86ff80e07317375cbc9081ffb63
lididimi <dimitrovalidia@yahoo.com>
2023-01-30 19:47:19 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -32,16 +32,6 @@ let activeUsers = []
 
 io.on("connection", socket  => {
 
-
-  // if(!activeUsers.some((user)=> user.id === socket.id)){
-  //   activeUsers.push({
-  //     // userId: newUserId,
-  //     socketId: socket.id
-  //   })
-  // }
-  // console.log('Connected Users', activeUsers)
-  // socket.emit('get-users', activeUsers) //io?
-
   // console.log('active', activeUsers)
   const typingController = new TypingController(socket)
   // const roomController = new RoomController(socket)
@@ -68,14 +58,20 @@ io.on("connection", socket  => {
     const {receiverId} = data
     const user = activeUsers.find((user) => user.userId === receiverId)
     
-    if(user) {
+    // if(user) {
       socket.to(user.socketId).emit('receive-message', data)
-    }
+    // }
     console.log('From socket to:', receiverId)
     console.log('Data',data)
   })
   
 
+  socket.on('disconnect', () => {
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id)
+    console.log('User left', activeUsers)
+    socket.emit('get-users', activeUsers) 
+  
+  })
 
 
 
@@ -85,12 +81,6 @@ io.on("connection", socket  => {
   // socket.on('typing-stopped',typingController.typingStopped)
   // socket.on('join chat', joinChatController.joinChat)
 
-  socket.on('disconnect', () => {
-    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id)
-    console.log('User left', activeUsers)
-    socket.emit('get-users', activeUsers) 
-  
-  })
 })
 
 
