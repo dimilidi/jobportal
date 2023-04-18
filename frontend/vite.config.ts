4b24c9f7835fb2edaa345202899960839f0f3574
dimilidi <dimitrovalidia@yahoo.com>
2023-04-19 01:28:44 +0200

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// transform mixed ES modules (i.e., modules that include both ESM and CommonJS syntax) to be compatible with the build process
 export default defineConfig({
   plugins: [react()],
 
